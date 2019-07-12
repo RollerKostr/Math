0a42990cfb31f2ac9e39e545987a067a161af3d0
RollerKostr <kmc1991@yandex.ru>
2019-07-12 20:41:57 +0300

    Added GCD for 2 args + some tests

--- src/RollerKostr.Utils.Math/GcdCalculator.cs
+++ src/RollerKostr.Utils.Math/GcdCalculator.cs
@@ -1,13 +1,38 @@
+using System;
+using System.Linq;
+
 namespace RollerKostr.Utils.Math
 {
     public static class GcdCalculator
     {
-        public static int CalculateGcd(params int[] arguments)
+        public static int Gcd(int a, int b)
         {
-            return 0;
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException(
+                    "Both arguments must be non-negative.");
+            }
+            
+            return b != 0
+                ? Gcd(b, a % b)
+                : a;
         }
         
-        public static double CalculateAgcd(double tolerancePercent, params double[] arguments)
+        public static int Gcd(params int[] arguments)
+        {
+            switch (arguments.Length)
+            {
+                case 0:
+                    throw new ArgumentException(
+                        $"Function {nameof(Gcd)}() requires at least 1 argument.");
+                case 1:
+                    return arguments[0];
+                default:
+                    return arguments.Aggregate(Gcd);
+            }
+        }
+        
+        public static double Agcd(double tolerancePercent, params double[] arguments)
         {
             return 0d;
         }
--- tests/RollerKostr.Utils.Math.Tests/GcdCalculatorTests.cs
+++ tests/RollerKostr.Utils.Math.Tests/GcdCalculatorTests.cs
@@ -1,21 +1,72 @@
+using FluentAssertions;
 using Xunit;
 
 namespace RollerKostr.Utils.Math.Tests
 {
     public class GcdCalculatorTests
     {
-        [Fact]
-        public void CalculateGcd_ShouldReturnGcd()
+        [Theory]
+        [InlineData( 0,  0, 0)]
+        [InlineData( 0,  1, 1)]
+        [InlineData( 1,  0, 1)]
+        [InlineData( 1,  1, 1)]
+        [InlineData( 1,  2, 1)]
+        [InlineData( 2,  3, 1)]
+        [InlineData( 3,  5, 1)]
+        [InlineData( 2,  4, 2)]
+        [InlineData( 6, 12, 6)]
+        [InlineData(12, 30, 6)]
+        public void Gcd_ShouldReturnGcd(int a, int b, int expectedGcd)
         {
             // Arrange
             
             // Act
-            
+            var gcd = GcdCalculator.Gcd(a, b);
+
             // Assert
+            gcd.Should().Be(expectedGcd);
+        }
+
+        [Theory]
+        [InlineData( 0,  1)]
+        [InlineData( 1,  0)]
+        [InlineData( 1,  1)]
+        [InlineData( 1,  2)]
+        [InlineData( 2,  3)]
+        [InlineData( 3,  5)]
+        [InlineData( 2,  4)]
+        [InlineData( 6, 12)]
+        [InlineData(12, 30)]
+        // TODO[mk] add random data generation + output logging
+        public void Gcd_ShouldReturnDivisorWithoutReminder(int a, int b)
+        {
+            // Arrange
+
+            // Act
+            var gcd = GcdCalculator.Gcd(a, b);
+
+            // Assert
+            (a % gcd).Should().Be(0);
+            (b % gcd).Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        public void Gcd_ShouldReturnGreatestDivisor(int a, int b)
+        {
+            // Arrange
+            
+            // Act
+            var gcd = GcdCalculator.Gcd(a, b);
+
+            // Assert
+            // TODO[mk] add check for greatest divisor
         }
         
         [Fact]
-        public void CalculateAgcd_ShouldReturnAgcd()
+        public void Agcd_ShouldReturnAgcd()
         {
             // Arrange
             
