b94404176aa2b7a3cb7c19c114d8c1d554a2c721
RollerKostr <kmc1991@yandex.ru>
2019-07-12 19:57:48 +0300

    Solution structure started

---
+++ src/RollerKostr.Utils.Math/GcdCalculator.cs
@@ -0,0 +1,15 @@
+namespace RollerKostr.Utils.Math
+{
+    public static class GcdCalculator
+    {
+        public static int CalculateGcd(params int[] arguments)
+        {
+            return 0;
+        }
+        
+        public static double CalculateAgcd(double tolerancePercent, params double[] arguments)
+        {
+            return 0d;
+        }
+    }
+}
---
+++ tests/RollerKostr.Utils.Math.Tests/GcdCalculatorTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+namespace RollerKostr.Utils.Math.Tests
+{
+    public class GcdCalculatorTests
+    {
+        [Fact]
+        public void CalculateGcd_ShouldReturnGcd()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Fact]
+        public void CalculateAgcd_ShouldReturnAgcd()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+    }
+}
